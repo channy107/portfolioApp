5aa424f598e260c5eddd0acd9d3fe61485d9131f
user <hunchan12@gmail.com>
2019-05-16 19:22:36 +0900

    Display Profiles

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import PrivateRoute from './components/routing/PrivateRoute';
+import Profiles from './components/profiles/Profiles';
 import './App.css';
 import Alert from './components/layout/Alert';
 // Redux
@@ -38,6 +39,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <Route exact path='/profiles' component={Profiles} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <PrivateRoute exact path='/create-profile' component={CreateProfile} />
               <PrivateRoute exact path='/edit-profile' component={EditProfile} />
