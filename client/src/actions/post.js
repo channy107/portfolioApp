6fa6ca96b5cef96912687758f7075263e49d7167
user <hunchan12@gmail.com>
2019-05-18 13:58:38 +0900

    Like & Unlike Functionality

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -3,7 +3,8 @@ import { setAlert } from './alert';
 import {
     GET_POSTS,
     POST_ERROR,
-    PROFILE_ERROR
+    PROFILE_ERROR,
+    UPDATE_LIKES
 } from './types';
 
 // Get posts
@@ -22,3 +23,39 @@ export const getPosts = () => async dispatch => {
         })
     }
 }
+
+// Add like
+export const addLike = id => async dispatch => {
+    try {
+        const res = await axios.put(`/api/posts/like/${id}`);
+
+        dispatch({
+            type: UPDATE_LIKES,
+            // id = post id
+            payload: { id, likes: res.data }
+        });
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+// Remove like
+export const removeLike = id => async dispatch => {
+    try {
+        const res = await axios.put(`/api/posts/unlike/${id}`);
+
+        dispatch({
+            type: UPDATE_LIKES,
+            // id = post id
+            payload: { id, likes: res.data }
+        });
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
