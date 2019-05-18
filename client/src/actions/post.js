04dc47b231c7322586d7ef6713cb7d81eb4bd0a6
user <hunchan12@gmail.com>
2019-05-18 16:07:21 +0900

    Comment Display & Delete

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -7,7 +7,9 @@ import {
     UPDATE_LIKES,
     DELETE_POST,
     ADD_POST,
-    GET_POST
+    GET_POST,
+    ADD_COMMENT,
+    REMOVE_COMMENT
 } from './types';
 
 // Get posts
@@ -124,3 +126,49 @@ export const getPost = id => async dispatch => {
         })
     }
 }
+
+// Add comment
+export const addComment = (postId, formData) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    try {
+        const res = await axios.post(`/api/posts/comment/${postId}`, formData, config);
+
+        dispatch({
+            type: ADD_COMMENT,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Comment Added', 'success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+// Delete comment
+// 어떤 게시물에 어떤 comment를 지울 지 postId, commentId
+export const deleteComment = (postId, commentId) => async dispatch => {
+
+    try {
+        const res = await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+
+        dispatch({
+            type: REMOVE_COMMENT,
+            payload: commentId
+        });
+
+        dispatch(setAlert('Comment Removed', 'success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
